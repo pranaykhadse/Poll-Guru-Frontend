195c1bfb48b550cbaea571cd08e8f06d8c2f3e86
pranaykhadse <pranay.handysolver@gmail.com>
2023-06-16 11:03:45 +0530

    added ShowResult Component

--- pollguru/src/Components/ShowResult/ShowResultComponents/RightView/RightView.jsx
+++ pollguru/src/Components/ShowResult/ShowResultComponents/RightView/RightView.jsx
@@ -8,9 +8,9 @@ function RightView({pollData,pollNo,timer,isContinue,handlePrevious,handleContin
     return (
         <Box sx={{ marginLeft: { xl: '15vw', lg: '15vw', md: '15vw', sm: '0px', xs: '0px' }, marginTop: { xl: '0px', lg: '0px', md: '0px', sm: '25vh', xs: '25vh' }, width: { xl: '84vw', lg: '84vw', md: '84vw', sm: '100vw', xs: '100vw' } }}>
             <QuestionBar pollData={pollData} pollNo={pollNo} timer={timer} />
-            {/* <Box component={'h3'} sx={{marginLeft:'220px',color:'black',textAlign:'left'}}>
-            Response : 
-        </Box> */}
+            <Box component={'h3'} sx={{color:'black',textAlign:'center',margin:'20px auto'}}>
+            Responses : 
+        </Box>
             <PrevContinueButtons pollData={pollData} pollNo={pollNo} isContinue={isContinue} handlePrevious={handlePrevious} handleContinue={handleContinue} />
         </Box>
     )
